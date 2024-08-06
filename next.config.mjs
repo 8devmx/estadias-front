f921a55a8f4d89726a3616d592314e602e4f1b91
jose ramon arias borges <jaborges666@gmail.com>
2024-08-06 12:38:24 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,compiler: {
-//     // Enables the styled-components SWC transform
-//     styledComponents: true
-//   }
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
+  reactStrictMode: true,compiler: {
     // Enables the styled-components SWC transform
-    styledComponents: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8000/:path*', // Proxy to Backend
-      },
-    ];
-  },
+    styledComponents: true
+  }
 };
 
 export default nextConfig;
