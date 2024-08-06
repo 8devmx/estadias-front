4c491cf2c23f8dc4c8bf6096750d8b995130399a
jose ramon arias borges <jaborges666@gmail.com>
2024-08-06 14:44:25 -0500

    actualizacion

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,compiler: {
-//     // Enables the styled-components SWC transform
-//     styledComponents: true
-//   }
-// };
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
+  reactStrictMode: true,compiler: {
     // Enables the styled-components SWC transform
-    styledComponents: true,
-  },
-  env: {
-    API_URL: process.env.API_URL || 'http://localhost:8000',
-  },
+    styledComponents: true
+  }
 };
 
 export default nextConfig;
---
+++ src/pages/Prueva.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+const Prueva = () => {
+  
+
+  return (
+    <div>
+
+        <h1>prueva de carga de imagenes</h1>
+
+        <img src="/back-technology.jpg" alt="" />
+        <img src="/unid-cancun.jpg" alt="" />
+
+    </div>
+  );
+}
+
+export default Prueva;
