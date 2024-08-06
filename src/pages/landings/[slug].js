0066f87868de124738ae8eea3927438138635515
jose ramon arias borges <jaborges666@gmail.com>
2024-08-06 14:58:19 -0500

    slug

--- src/pages/landings/[slug].js
+++ src/pages/landings/[slug].js
@@ -1,3 +1,54 @@
+// import Hero from '@/components/hero';
+// import Navbar from '@/components/navbar';
+// import Services from '@/components/services';
+// import Packages from '@/components/packages';
+// import Form from '@/components/form';
+
+// export async function getServerSideProps(context) {
+//   const { slug } = context.params;
+//   const url = `http://localhost:8000/landingslg/slug/${slug}`;
+//   // const url = `${process.env.API_URL}/landingslg/slug/${slug}`;
+
+//   try {
+//     const res = await fetch(url);
+
+//     if (!res.ok) {
+//       return { notFound: true }; 
+//     }
+
+//     const data = await res.json();
+
+//     if (!data) {
+//       return { notFound: true }; 
+//     }
+
+//     return {
+//       props: { landing: data }, 
+//     };
+//   } catch (error) {
+//     return { notFound: true }; 
+//   }
+// }
+
+// export default function Home({ landing }) {
+//   const hero = JSON.parse(landing.hero);
+//   const services = JSON.parse(landing.services);
+//   const packages = JSON.parse(landing.packages);
+//   const logo = landing.logo;
+
+//   return (
+//     <>
+//       <Hero data={hero} />
+//       <Navbar logo={logo} />
+//       <Services data={services} />
+//       <Packages data={packages} />
+//       <Form />
+//     </>
+//   );
+// }
+
+
+
 import Hero from '@/components/hero';
 import Navbar from '@/components/navbar';
 import Services from '@/components/services';
@@ -10,23 +61,29 @@ export async function getServerSideProps(context) {
   // const url = `${process.env.API_URL}/landingslg/slug/${slug}`;
 
   try {
+    console.log(`Fetching data from: ${url}`); // Log de la URL de la API
+
     const res = await fetch(url);
 
     if (!res.ok) {
-      return { notFound: true }; 
+      console.error(`Error fetching data: ${res.status} ${res.statusText}`); // Log del error HTTP
+      return { notFound: true };
     }
 
     const data = await res.json();
+    console.log(`Data received: ${JSON.stringify(data)}`); // Log de los datos recibidos
 
     if (!data) {
-      return { notFound: true }; 
+      console.error('No data received or data is null/undefined'); // Log de datos vacíos
+      return { notFound: true };
     }
 
     return {
       props: { landing: data }, 
     };
   } catch (error) {
-    return { notFound: true }; 
+    console.error(`Fetch error: ${error.message}`); // Log del error de fetch
+    return { notFound: true };
   }
 }
 
