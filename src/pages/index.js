b4a046c299540140886dfc164c6721fb1b842c1e
jose ramon arias borges <jaborges666@gmail.com>
2024-08-06 15:33:21 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,29 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
+// Redirección del lado del servidor
+export async function getServerSideProps(context) {
+  return {
+    redirect: {
+      destination: '/indexP', // Reemplaza '/new-page' con la ruta a la que quieres redirigir
+      permanent: false, // Si es true, indica que la redirección es permanente (301)
+    },
+  };
+}
+
 export default function Home() {
   return (
     <div>
       <Head>
+        <title>Redirect Example</title>
+        <meta name="description" content="Redirect example with Next.js" />
       </Head>
 
       <main>
-        <h1>HOLA MUNDO</h1>
+        <h1>cargando</h1>
       </main>
 
       <footer>
-        <p>Powered by Next.js</p>
       </footer>
     </div>
   );
