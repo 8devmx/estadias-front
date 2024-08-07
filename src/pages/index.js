2cc25ea47ed6a55f8237e02f30e2896437bd25fd
jose ramon arias borges <jaborges666@gmail.com>
2024-08-06 19:05:31 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,33 @@
 import Hero from '@/components/heromain';
 import Navbar from '@/components/navbarmain';
 
-// export async function getServerSideProps(context) {
+export async function getServerSideProps(context) {
 
-// const url = `http://localhost:8000/landingslg/slug/tech-pech`;
+const url = `${process.env.NEXT_PUBLIC_API_KEY}/landingslg/slug/tech-pech`;
 
-//   try {
-//     const res = await fetch(url);
+  try {
+    const res = await fetch(url);
 
-//     if (!res.ok) {
-//       return { notFound: true }; 
-//     }
+    if (!res.ok) {
+      return { notFound: true }; 
+    }
 
-//     const data = await res.json();
+    const data = await res.json();
 
-//     if (!data) {
-//       return { notFound: true }; 
-//     }
+    if (!data) {
+      return { notFound: true }; 
+    }
 
-//     return {
-//       props: { landing: data }, 
-//     };
-//   } catch (error) {
-//     return { notFound: true }; 
-//   }
-// }
+    return {
+      props: { landing: data }, 
+    };
+  } catch (error) {
+    return { notFound: true }; 
+  }
+}
 
 export default function Home({ landing }) {
-  // const hero = JSON.parse(landing.hero);
-  const hero = JSON.parse('{"background": "back-technology.jpg", "title": "Tech-Pech a la velocidad de la tecnología", "paragraph": "Soluciones Técnicas de Confianza para tu Equipo de Cómputo", "buttonText": "Solicita tu Servicio", "buttonLink": "https://www.google.com"}');
+  const hero = JSON.parse(landing.hero);
   const logo = landing.logo;
 
   return (
