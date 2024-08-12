7f7d01da730fa05ede37e69d0ecfcd59d7c1525a
CarlosEstrella <43283948+CarlosEstrella@users.noreply.github.com>
2024-08-12 16:42:47 -0500

    cambios esteticos

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -33,8 +33,8 @@ const Navbar = ({ logo }) => {
       <nav>
         <ul>
           <li><a href="#home">Home</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#packages">Packages</a></li>
+          <li><a href="#services">Servicios</a></li>
+          <li><a href="#packages">Paquetes</a></li>
         </ul>
       </nav>
     </div>
